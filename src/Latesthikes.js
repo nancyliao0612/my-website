b8d453e266eb3801fb0a055a48489df9f2e59c4f
Nancy <nancy62924@gmail.com>
2021-09-03 18:02:31 +0800

    home page - hiking articles finished

--- src/Latesthikes.js
+++ src/Latesthikes.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import hiking_pic from "./picture/hiking-大霸.JPG"
+import React from "react";
+import hiking_pic from "./picture/hiking-大霸.JPG";
+import hiking_pic2 from "./picture/hiking-閂山.JPG";
+import hiking_pic3 from "./picture/hiking-南湖.JPG";
 
 const Latesthikes = () => {
   return (
@@ -10,33 +12,89 @@ const Latesthikes = () => {
       </div>
       <div className="article-section">
         <div className="article-container">
-          <img src={hiking_pic} alt="pic" className="hiking-pic"/>
+          <img src={hiking_pic} alt="pic" className="hiking-pic" />
           <div className="mountain-container">
-            <h2 className="mountain-style">大霸尖山</h2>
-            <div className="name-style">
-            <div className="text-center">
-              <span>大霸尖山</span>
-              <p>3492m</p>
+            <div className="name-style-1">
+              <h2 className="mountain-style">大霸尖山</h2>
+              <h6>Date: 2021/4/20~2021/4/22</h6>
             </div>
-            <div className="text-center">
-              <span>小霸尖山</span>
-              <p>3492m</p>
-            </div>
-            <div className="text-center">
-              <span>伊澤山</span>
-              <p>3492m</p>
-            </div>
-            <div className="text-center">
-              <span>加利山</span>
-              <p>3492m</p>
-            </div>
+            <div className="name-style-2">
+              <div className="text-center color-under">
+                <h5>大霸尖山</h5>
+                <p>(3492m)</p>
+              </div>
+              <div className="text-center color-under">
+                <h5>小霸尖山</h5>
+                <p>(3418m)</p>
+              </div>
+              <div className="text-center color-under-2">
+                <h5>伊澤山</h5>
+                <p>(3297m)</p>
+              </div>
+              <div className="text-center color-under-2">
+                <h5>加利山</h5>
+                <p>(3112m)</p>
+              </div>
             </div>
           </div>
         </div>
-        
+      </div>
+
+      {/* 第二篇 */}
+      <div className="article-section">
+        <div className="article-container">
+          <img src={hiking_pic2} alt="pic" className="hiking-pic" />
+          <div className="mountain-container">
+            <div className="name-style-1">
+              <h2 className="mountain-style">閂山鈴鳴</h2>
+              <h6>Date: 2021/4/20~2021/4/22</h6>
+            </div>
+            <div className="name-style-2">
+              <div className="text-center color-under-3">
+                <h5>鈴鳴山</h5>
+                <p>(3272m)</p>
+              </div>
+              <div className="text-center color-under-3">
+                <h5>閂山</h5>
+                <p>(3168m)</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* 第三篇 */}
+      <div className="article-section">
+        <div className="article-container">
+          <img src={hiking_pic3} alt="pic" className="hiking-pic" />
+          <div className="mountain-container">
+            <div className="name-style-1">
+              <h2 className="mountain-style">南湖大山</h2>
+              <h6>Date: 2021/4/20~2021/4/22</h6>
+            </div>
+            <div className="name-style-2">
+              <div className="text-center color-under">
+                <h5>南湖北山</h5>
+                <p>(3536m)</p>
+              </div>
+              <div className="text-center color-under">
+                <h5>南湖主峰</h5>
+                <p>(3740m)</p>
+              </div>
+              <div className="text-center color-under-2">
+                <h5>南湖東峰</h5>
+                <p>(3632m)</p>
+              </div>
+              <div className="text-center color-under-2">
+                <h5>審馬陣山</h5>
+                <p>(3141m)</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Latesthikes
+export default Latesthikes;
