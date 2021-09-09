0e3dc233182d7712b4ca27387603384ec283abdf
Nancy <nancy62924@gmail.com>
2021-09-09 15:41:45 +0800

    DB - picture layout

--- src/HikingDB.js
+++ src/HikingDB.js
@@ -1,4 +1,12 @@
 import React from "react";
+import {
+  FaStar,
+  FaRegStar,
+  FaStarHalfAlt,
+  FaMountain,
+  FaRoad,
+} from "react-icons/fa";
+import { AiFillClockCircle } from "react-icons/ai";
 
 const HikingDB = () => {
   return (
@@ -7,7 +15,21 @@ const HikingDB = () => {
         <img src="https://i.imgur.com/3IHD5rl.png" alt="" className="map-pic" />
       </div>
       <div className="DB-center">
-        <p className="text-intro">
+        <div className="summary-container">
+          <div className="summary time">
+            <AiFillClockCircle className="total-icons" />
+            ：3 天
+          </div>
+          <div className="summary">
+            <FaRoad className="total-icons" />
+            ：62 公里
+          </div>
+          <div className="summary">
+            <FaMountain className="total-icons" />
+            ：4 座百岳
+          </div>
+        </div>
+        <p>
           大霸尖山位於雪山山脈，自古是泰雅族的聖山，傳說是祖先的發源地，相傳大洪水時泰雅祖先爬到大霸躲過了洪水，並在大霸的周邊生活數百年。四面懸岩峭壁的大霸尖山，遠眺形狀酷似酒桶，因此早期漢族稱之為「酒桶山」，除此之外，山勢磅礡奇特，又有「世
           紀奇峰」的美譽。
           <br />
@@ -41,7 +63,88 @@ const HikingDB = () => {
             大霸登山服務站
           </span>
           <br />
+          <span>
+            整體難易度：
+            <div className="star">
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStarHalfAlt />
+              <FaRegStar />
+              <p className="text-small">（提供給熟知楠西體力的朋友當參考）</p>
+            </div>
+          </span>
         </div>
+        <h3>入園證/入山證</h3>
+        <ul className="text-bullet">
+          <li>申請網址：台灣國家公園入園入山線上申請（雪霸國家公園 大霸線）</li>
+          <li>申請時間：入園日前 5 日至入園日前 2 個月每日 07:00-23:00 提出</li>
+          <li>
+            申請制度：雪霸國家公園採行「先搶先贏」，非抽籤制！（盡量提早 45
+            前申請才保險哦！）
+          </li>
+          <li>
+            注意事項：
+            <li className="bullet-tab">
+              入園申請會綁定山屋資格，隨時注意是否收到九九山莊繳費通知的信件。
+            </li>
+            <li className="bullet-tab">
+              入園許可證需列印兩份，一份投遞至大霸登山服務口信箱，另一份給九九山莊的人員查看。
+            </li>
+          </li>
+          <li>
+            side note: 申請規則時常更動，最新情況還是要依照國家公園公告為主！
+          </li>
+        </ul>
+        <h3>九九山莊</h3>
+        <ul className="text-bullet">
+          九九山莊海拔 2,699
+          公尺，要爬大小霸的山友通常會選擇在此住宿兩個晚上。（體力較好的也可以選擇第一天住中霸山屋）
+          <li>床位：九九山莊床位查詢</li>
+          <li>網路：全區皆有網路，收訊良好</li>
+          <li>費用：$200 / 晚（需事先匯款）</li>
+          <li>
+            注意事項：
+            <li className="bullet-tab">
+              注意事項：龍門客棧的通鋪會有老鼠出沒，糧食記得要密封好。
+            </li>
+          </li>
+        </ul>
+        <div className="underline-article"></div>
+        <h3>Day 1</h3>
+        <p>
+          第一天凌晨 2:30 從台北開車出發，抵達大霸登山服務站的時間是早上 6:30。
+          建議可以先在服務站上洗手間，不然下個公廁就是 19
+          公里處，要到馬達拉西登山口附近。
+          <br />
+          越過管制口的柵欄後，就是長達 19 公里的「大鹿林道東線」。
+          <br />
+          <br />
+          左右林蔭遮蔽住大部分的陽光，一路走來不會很曝曬也十分平緩。
+          <br />
+        </p>
+        <img src="https://i.imgur.com/QBMMoP6.jpg" alt="" />
+        <img src="https://i.imgur.com/tSYyE93.jpg" alt="" />
+        <img src="https://i.imgur.com/66xwzi2.jpg" alt="" />
+        <img src="https://i.imgur.com/JwJsKtG.jpg" alt="" />
+        <img src="https://i.imgur.com/nLYcpiq.jpg" alt="" />
+        <img src="https://i.imgur.com/RWnc8ZF.png" alt="" />
+        <img src="https://i.imgur.com/MVN8Wii.jpg" alt="" />
+        <img src="https://i.imgur.com/8ouaCsc.jpg" alt="" />
+        <img src="https://i.imgur.com/EBm1qeW.jpg" alt="" />
+        <div className="img-double">
+          <img src="https://i.imgur.com/xdDV437.jpg" alt="" />
+          <img src="https://i.imgur.com/geg1mqT.jpg" alt="" />
+        </div>
+        <img src="https://i.imgur.com/sEbR78w.jpg" alt="" />
+        <img src="https://i.imgur.com/qGjzLCF.jpg" alt="" />
+        <img src="https://i.imgur.com/YtUK4qx.jpg" alt="" />
+        <img src="https://i.imgur.com/iaB0Rre.jpg" alt="" />
+        <div className="img-double">
+          <img src="https://i.imgur.com/prcZ8He.jpg" alt="" />
+          <img src="https://i.imgur.com/Mz7F4Sx.jpg" alt="" />
+        </div>
+        <img src="https://i.imgur.com/7q6Jk9r.jpg" alt="" />
       </div>
     </>
   );
