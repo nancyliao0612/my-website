86ea68148ee396d27733531f4017866a07746706
Nancy <nancy62924@gmail.com>
2022-04-03 23:37:10 +0800

    sidebar refactor - part2

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { AppContext, useGlobalContext } from "../context/MenuContext";
 import { ThemeContext } from "../context/ThemeContext";
+import { links } from "../data/menuData";
 
 const Navbar = () => {
   const { openMenu } = useGlobalContext();
@@ -10,24 +11,39 @@ const Navbar = () => {
     <nav className={theme}>
       <div className="nav-center" id="home">
         <div className="nav-header">
-          <a href="/">
-            <img
-              src="https://i.imgur.com/a3AIbE4.png?1"
-              className="logo"
-              alt="logo"
-            />
-          </a>
-          <div className="toggler">
-            <p className="toggler--light">Light</p>
-            <div className="toggler--slider" onClick={toggleTheme}>
-              <div className="toggler--slider--circle"></div>
+          <div className="nav-left">
+            <a href="/">
+              <img
+                src="https://i.imgur.com/a3AIbE4.png?1"
+                className="logo"
+                alt="logo"
+              />
+            </a>
+            <div className="toggler">
+              <p className="toggler--light">Light</p>
+              <div className="toggler--slider" onClick={toggleTheme}>
+                <div className="toggler--slider--circle"></div>
+              </div>
+              <p className="toggler--dark">Dark</p>
             </div>
-            <p className="toggler--dark">Dark</p>
-            <button className="nav-toggle" onClick={openMenu}>
-              <AiOutlineMenu />
-            </button>
           </div>
+          <button className="nav-toggle" onClick={openMenu}>
+            <AiOutlineMenu />
+          </button>
         </div>
+        <ul className="nav-links">
+          {links.map((link) => {
+            const { id, url, text, icon } = link;
+            return (
+              <li key={id}>
+                <a href={url}>
+                  {icon}
+                  {text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   );
